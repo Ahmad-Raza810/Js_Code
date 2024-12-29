35006bea4820205a87e4be70443fa2afc5cb9d9e
Ahmad Raza <mustafaraza03898@gmail.com>
2024-12-29 09:53:25 +0530

    adding variable.js

---
+++ variable.js
@@ -0,0 +1,17 @@
+//concept of hoisting
+//var>>>>>global and functional scope
+//let and const >>>>>>>local and block scope
+//console.log(vari)
+vari=10
+myfunc=function(){
+var myVar=10
+let mylet=10 
+const myConst=10
+console.log(myVar,mylet,myConst,vari)
+}
+console.log(vari)
+myfunc()
+//console.log(myVar)
+{
+    console.log(vari) 
+}
