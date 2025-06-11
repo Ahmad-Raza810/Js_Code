ae17e3f957acf028f66b5774123a440f26f0a044
Ahmad Raza <mustafaraza03898@gmail.com>
2025-06-11 22:16:57 +0530

    behaviour of function.

--- t1.js
+++ t1.js
@@ -6,7 +6,21 @@ let   test2;
 const test3="val";
 console.log(test,test2,test3)
 
+
+var stop=5
+
+
+//By Default A function return undefined
 function greet() {
     console.log("hello from js function")
-    
+    stop--;
+    if(stop==1)
+    return
+    greet()   
 }
+
+console.log(typeof greet());
+
+
+
+
